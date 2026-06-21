a9ce0414fe3e9004090e6f7e5b8db8fb5d5a6e1c
rushita <rushita.work@gmail.com>
2026-06-21 13:30:35 -0400

    Add User model and update Todo and SubTodo schemas

--- models/todos/todo.models.js
+++ models/todos/todo.models.js
@@ -1,7 +1,12 @@
 import mongoose from "mongoose";
 
 const todoSchema = new mongoose.Schema({
-    
+    content:{type: String, required: true},
+    isComplete:{type: Boolean, default: false},
+    CreatedBy:{type: mongoose.Schema.Types.ObjectId, ref: "User", required: true}, 
+    subTodos: [{
+        type: mongoose.Schema.Types.ObjectId, ref: "SubTodo"
+    }] // array of sub-todo references
 }, {timestamps: true});
 
 export const Todo = mongoose.model("Todo", todoSchema);
