9fdec43f7bd0803ed68cf0dd3015c8dca09ce01d
rushita <rushita.work@gmail.com>
2026-06-23 06:57:55 -0400

    setup project files

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-// const app = require('express')();
 
-import express from 'express';
-const app = express();
-const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World, Rush!');
-});
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
+
+
+
+// // const app = require('express')();
+
+// import express from 'express';
+// const app = express();
+// const PORT = 3000;
+
+// app.get('/', (req, res) => {
+//   res.send('Hello, World, Rush!');
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
