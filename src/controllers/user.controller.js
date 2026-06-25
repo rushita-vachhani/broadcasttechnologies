0aa09965dcabead499fb7d67cfc018e1534e26ac
rushita <rushita.work@gmail.com>
2026-06-25 16:46:53 -0400

    Implement user registration logic and update routes to handle file uploads

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,20 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-    // Your registration logic here
-    res.status(201).json({ 
-        message: "User registered successfully" 
-    });
+    // ask user to share data
+    // get data from user and validate if all the required fields are present + check for avatar and images
+    // check if user already exists: check username and email
+    // if user exists then show message like already exists
+    // upload image to cloudinary + avatar
+    // if not exists then create user object and create entry in DB
+    // remove password and refresh token from response
+    // check for user creation
+    // return response
+   
+    const {fullName, username, email, password} = req.body;
+    console.log(fullName, username, email, password);
+    
+
 });
 
 export { registerUser };
