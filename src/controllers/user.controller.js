865b8f900999857c952568d461959268ad1d2b45
rushita <rushita.work@gmail.com>
2026-06-28 09:52:30 -0400

    Fix import statement for JsonWebTokenError and update refreshAccessToken logic to log incoming token

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.models.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { JsonWebTokenError } from "jsonwebtoken";
+import JsonWebTokenError from "jsonwebtoken";
 
 const  generateAccessAndRefreshTokens = async(userId) => {
     try {
@@ -176,7 +176,8 @@ const logoutUser = asyncHandler( async (req, res) => {
 
 const refreshAccessToken = asyncHandler( async (req, res) => {
     const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
-    if (incomingRefreshToken) {
+    console.log(`incomingRefreshToken is........... ${incomingRefreshToken}`);
+    if (!incomingRefreshToken) {
         throw new ApiError(401,"To get new token, Need to pass refreshToken so unauthorized request!")
     }
 
