e24bcd0d63de4d73f8414f7c6ae860320e327a4c
rushita <rushita.work@gmail.com>
2026-07-16 17:14:22 -0400

    Add getWatchHistory functionality to retrieve user's watch history with video owner details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -357,6 +357,52 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
 
 });
 
+const getWatchHistory = asyncHandler(async (req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        }, {
+            $lookup:{
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline:[
+                                {
+                                    $project:{
+                                        fullName: 1,
+                                        avatar: 1,
+                                        username: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {$first: "$owner"}
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res.status(200)
+            .json(new ApiResponse(200, user[0]?.watchHistory, "Watch history fetched successfully!"));
+});
+
+
+
 export { registerUser, 
         loginUser,
         logoutUser,
@@ -365,4 +411,6 @@ export { registerUser,
         getCurrentUser,
         updateAccountDetails,
         updateUserAvatar,
-        getUserChannelProfile };
+        getUserChannelProfile,
+        getWatchHistory
+     };
